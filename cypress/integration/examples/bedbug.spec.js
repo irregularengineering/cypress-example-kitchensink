1eaceccceba4b5e323067844c29637a345d9b3b3
Paul Karayan <paulkarayan@gmail.com>
2019-08-07 15:58:43 -0700

    she works

--- cypress/integration/examples/bedbug.spec.js
+++ cypress/integration/examples/bedbug.spec.js
@@ -23,7 +23,7 @@ context('Aliasing', () => {
   it('doesnt fail when error is injected because of poor test design', () => {
     // now let's bedbug this mother
     cy.server()
-    
+
     cy.route({
       method: 'GET',      // Route all GET requests
       url: 'comments/*',    // that have a URL that matches '/users/*'
@@ -34,26 +34,26 @@ context('Aliasing', () => {
 
     // cy.wait('@getBadComment').its('status').should('eq', 200)
     cy.get('.network-comment').should('contain', 'dolor')
-      })
+  })
 
-    it('should fail when error is injected', () => {
-      // now let's bedbug this mother
-      cy.server()
-      
-      cy.route({
-        method: 'GET',      // Route all GET requests
-        url: 'comments/*',    // that have a URL that matches '/users/*'
-        status: 412,
-        response: {
-          // simulate a redirect to another page
-          redirect: '/error'
-        }          
-      }).as('getBadComment')
-  
-      cy.get('.network-btn').click()
-  
-      cy.wait('@getBadComment').its('status').should('eq', 200)
-      cy.get('.network-comment').should('contain', 'dolor')
-        })
+  it('should fail when error is injected', () => {
+    // now let's bedbug this mother
+    cy.server()
+
+    cy.route({
+      method: 'GET',      // Route all GET requests
+      url: 'comments/*',    // that have a URL that matches '/users/*'
+      status: 412,
+      response: {
+        // simulate a redirect to another page
+        redirect: '/error'
+      }
+    }).as('getBadComment')
+
+    cy.get('.network-btn').click()
+
+    cy.wait('@getBadComment').its('status').should('eq', 200)
+    cy.get('.network-comment').should('contain', 'dolor')
+  })
 
 })
